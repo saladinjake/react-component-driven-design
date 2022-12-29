5fe9941c29e77be85c29d06ddb5f4e424c96cf7f
victorjuwa <victor.juwa@kuda.com>
2022-12-29 20:34:59 +0100

    ch: (setup: scafold react typescript scss application with hooks )

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="mt-5">
+          <div className="container-fluid">
+            <div className="d-flex flex-row">
+              <div className="col-sm-12">
+                 Build somethin awesome
+              </div>
+            </div>
+          </div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data/testdata.ts
@@ -0,0 +1,7 @@
+import { Data } from "types";
+
+export const mockData: Data[] = [
+
+];
+
+export const sampleData = mockData[0];
