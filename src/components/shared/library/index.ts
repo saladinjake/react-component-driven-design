447f32f8e5425e430c8c447a32cec691f3e4c199
victorjuwa <victor.juwa@kuda.com>
2022-12-31 15:19:15 +0100

    bug-fix(ch: add flex components to export)

---
+++ src/components/shared/library/index.ts
@@ -0,0 +1,32 @@
+import Avatar from "./components/Avatar";
+import Button from "./components/Button";
+import Box from "./components/Box";
+import Input from "./components/Input";
+import Heading from "./components/Typography/Heading";
+import { Grid, GridItem } from "./components/Grid";
+import Select from "./components/Select";
+import Radio from "./components/Radio";
+import SearchField from "./components/SearchField";
+import DatePicker from "./components/DatePicker";
+// import Status from "./components/Status";
+import MultiSelect from "./components/MultiSelect";
+import Select2 from "./components/Select2";
+import Textarea from "./components/Textarea";
+import Flex from "./components/Flex-v1";
+export {
+  Avatar,
+  Button,
+  Box,
+  Flex,
+  Input,
+  Heading,
+  Grid,
+  GridItem,
+  Select,
+  Radio,
+  SearchField,
+  DatePicker,
+  MultiSelect,
+  Select2,
+  Textarea,
+};
