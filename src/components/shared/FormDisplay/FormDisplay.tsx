e000d689b2d1124c2773128209df31daea353552
victorjuwa <victor.juwa@kuda.com>
2022-12-31 10:05:45 +0100

    sample form display wrapper component implementation started

---
+++ src/components/shared/FormDisplay/FormDisplay.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FormDisplay() {
+  return <div>FormDisplay</div>;
+}
+
+export default FormDisplay;
---
+++ src/components/shared/FormDisplay/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const FormDisplay = () => {
+  return <div>FormDisplay</div>;
+};
+
+export default FormDisplay;
