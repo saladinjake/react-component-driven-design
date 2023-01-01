fd70725ddda2566548da6209333a2d09b90c6ad1
victorjuwa <victor.juwa@kuda.com>
2023-01-01 06:06:59 +0100

    sample login layout

--- src/modules/Login/index.tsx
+++ src/modules/Login/index.tsx
@@ -1,5 +1,34 @@
+const RightSection = () =>{
+    return (
+    <div className="rightside">
+    <h1>The Tale of the Heike</h1>
+    <p>
+      The sound of the Gion Shōja bells echoes the impermanence of all things; the color of the sāla flowers reveals the truth that the prosperous must decline. The proud do not endure, they are like a dream on a spring night; the mighty fall at last, they are as dust before the wind.
+    </p>
+ 
+  </div>
+    )
+
+}
+
+const LeftSection = () =>{
+return(
+    <div className="leftside">
+    <h1>The Tale of the Heike</h1>
+    <p>
+      The sound of the Gion Shōja bells echoes the impermanence of all things; the color of the sāla flowers reveals the truth that the prosperous must decline. The proud do not endure, they are like a dream on a spring night; the mighty fall at last, they are as dust before the wind.
+    </p>
+
+  </div>
+)
+}
+
+
 const Login = () =>{
-  return <>Now we begin</>
+  <div className="flexbox">
+    <LeftSection/>
+    <RightSection/>
+  </div>
 }
 
 export default Login
